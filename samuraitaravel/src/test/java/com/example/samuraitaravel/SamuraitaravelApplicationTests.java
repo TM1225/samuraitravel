555be7ba5537828daa1b2d2d426eb6aefd06813a
tm119 <tm119@LAPTOP-E67A6TEC>
2025-03-03 11:57:59 +0900

    初回プッシュ

---
+++ samuraitaravel/src/test/java/com/example/samuraitaravel/SamuraitaravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samuraitaravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuraitaravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
